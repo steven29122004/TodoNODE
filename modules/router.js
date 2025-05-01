abe93bfa50b4c2a07021e45a92be668d539c50dd
steven29122004 <165149053+steven29122004@users.noreply.github.com>
2025-05-01 15:39:31 +0700

    ini

--- modules/router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Controller = require('./controller');
-
-router.get('/', Controller.getPage);
-
-module.exports = router;
